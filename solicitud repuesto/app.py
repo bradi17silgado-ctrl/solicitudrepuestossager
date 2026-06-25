49f0749c9a00ab288ebf795251808c0c0cf59f78
sager <bradi17silgado@gmail.com>
2026-06-25 13:24:20 -0500

    Fix render

--- solicitud repuesto/app.py
+++ solicitud repuesto/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import mysql.connector
 import requests
+import os
 
 app = Flask(__name__)
 app.secret_key = "123456789003"
 
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
